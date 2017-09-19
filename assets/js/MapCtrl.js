7fd94c456d04ae2921742e12d4583f220557131f
Kevin Moore <kevinmoore@Kevins-MacBook-Air.local>
2017-09-19 23:03:36 +0100

    upgrade

---
+++ assets/js/MapCtrl.js
@@ -0,0 +1,82 @@
+//Data
+var cities = [
+    {
+        city : 'Cork',
+        desc : 'This is the best city in the world!',
+        lat : 51.896647,
+        long : -8.474801
+    },
+    {
+        city : 'New York',
+        desc : 'This city is aiiiiite!',
+        lat : 40.6700,
+        long : -73.9400
+    },
+    {
+        city : 'Chicago',
+        desc : 'This is the second best city in the world!',
+        lat : 41.8819,
+        long : -87.6278
+    },
+    {
+        city : 'Los Angeles',
+        desc : 'This city is live!',
+        lat : 34.0500,
+        long : -118.2500
+    },
+    {
+        city : 'Las Vegas',
+        desc : 'Sin City...\'nuff said!',
+        lat : 36.0800,
+        long : -115.1522
+    }
+];
+
+
+
+angular.module('mapsApp', ['uiGmapgoogle-maps']).controller('MapCtrl', function($scope) {
+    // Map Settings //
+    $scope.initialise = function() {
+        var myLatlng = new google.maps.LatLng(51.897849, -8.475614);
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      // Geo Location /
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: "My Location"
+            });
+        });
+        $scope.map = map;
+        // Additional Markers //
+        $scope.markers = [];
+        var infoWindow = new google.maps.InfoWindow();
+        var createMarker = function (info){
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(info.lat, info.long),
+                map: $scope.map,
+                animation: google.maps.Animation.DROP,
+                title: info.city
+            });
+            marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+            google.maps.event.addListener(marker, 'click', function(){
+                infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                infoWindow.open($scope.map, marker);
+            });
+            $scope.markers.push(marker);
+        }  
+        for (i = 0; i < cities.length; i++){
+            createMarker(cities[i]);
+        }
+
+    };
+    google.maps.event.addDomListener(document.getElementById("map"), 'load', $scope.initialise());
+
+});
